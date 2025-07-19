d30511a7a1743c170f49a4aebe33a82431bd936c
sav-chris <christopher.savini@gmail.com>
2025-07-19 23:04:30 +1000

    House keeping

--- COQ/examples.v
+++ COQ/examples.v
@@ -266,3 +266,78 @@ Qed.
 
 
 (*********************************)
+
+
+Lemma simplify_ineq_1 :
+  forall (a b : R),
+    a ≠ 0 ->
+    ((-1 / 2) ^ 2 * b ^ 2 + (-1 / 2) * b ^ 2) / a = (-1 / 4 * b ^ 2 / a).
+Proof.
+  intros a b H.
+  field.
+  assumption.
+Qed.
+
+
+Lemma simplify_ineq :
+  forall (a beta : R),
+    a ≠ 0 ->
+    -8 * beta ^ 2 / (8 * a) = - beta ^ 2 / a.
+Proof.
+  intros a beta H.
+  field.
+  assumption.
+Qed.
+
+
+Lemma simplify_ineq_2 :
+  forall (a b : R),
+    a ≠ 0 ->
+    ((-1 / 2) ^ 2 * b ^ 2 + (-1 / 2) * b ^ 2) / a = - (b ^ 2) / (4 * a).
+Proof.
+  intros a b H.
+  field.
+  assumption.
+Qed.
+
+
+Lemma simplify_ineq_3 :
+  forall (a b : R),
+    a ≠ 0 ->
+    -2 * b ^ 2 / (8 * a) = - b ^ 2 / (4 * a).
+Proof.
+  intros a b H.
+  field.
+  assumption.
+Qed.
+
+
+Lemma simplify_ineq_4 :
+  forall (a b : R),
+    a ≠ 0 ->
+    - b ^ 2 / (4 * a) <= - b ^ 2 / (4 * a).
+Proof.
+  intros a b H.
+  apply Rle_refl.
+Qed.
+
+
+Lemma simplify_ineq_5 :
+  forall (a b : R),
+    a ≠ 0 ->
+    -2 * b ^ 2 / (8 * a) <= -2 * b ^ 2 / (8 * a).
+Proof.
+  intros a b H.
+  apply Rle_refl.
+Qed.
+
+Lemma pos_implies_nonzero :
+  forall (a : R), 0 < a -> a <> 0.
+Proof.
+  intros a H.
+  lra.
+Qed.
+
+(*********************************************)
+
+
